0de17658c5dda17986499e3e839159314dd809d2
Eslam <eslam.mohamed.551999@gmail.com>
2025-06-11 18:23:14 +0300

    update assets

---
+++ app/src/main/java/com/example/pizzaoven/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.example.pizzaoven.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.pizzaoven.R
+
+val SFPro = FontFamily (
+    Font(R.font.sf_pro_regular, weight = FontWeight.Normal),
+    Font(R.font.sf_pro_medium, weight = FontWeight.Medium),
+    Font(R.font.sf_pro_bold, weight = FontWeight.Bold),
+)
